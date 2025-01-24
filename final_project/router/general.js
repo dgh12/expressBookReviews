4b229e2b96f85d60cf7f7ea691f76b3d23522b41
DGH12 <dghedding@gmail.com>
2025-01-24 10:50:44 -0600

    the project is done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,50 +26,63 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user." + os.EOL});
 });
 
-
-
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   // Send JSON response with formatted books data
-  res.send(JSON.stringify(books,null,4) + os.EOL);
+  const get_books = new Promise((resolve,reject) => {
+    resolve(JSON.stringify(books,null,4) + os.EOL)
+  })
+  get_books.then((successMessage) => {
+    res.send(successMessage)
+  })
 });
-
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  // Retrieve the email parameter from the request URL and send the corresponding friend's details
-  const ISBN = req.query.isbn;
-  const book = books.filter(book => book.isbn === ISBN);
-  res.send(book);
+  const get_books_by_isbn = new Promise((resolve,reject) => {
+    const ISBN = req.query.isbn
+    // Retrieve the email parameter from the request URL and send the corresponding friend's details
+   resolve(books.filter(book => book.isbn === ISBN))
+ })
+  get_books_by_isbn.then((successMessage) => {
+    res.send(successMessage)
+  })
  });
  
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const Author = req.query.author
-  const book = books.filter(book => book.author === Author);
-  res.send(book);
-  res.send(os.EOL);
+  const get_books_by_author = new Promise((resolve,reject) => {
+    const Author = req.query.author
+    resolve(books.filter(book => book.author === Author))
+  })
+  get_books_by_author.then((successMessage) => {
+  res.send(successMessage)
+  })
 });
 
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const Title = req.query.title
-  const book = books.filter(book => book.title === Title);
-  res.send(book);
-  res.send(os.EOL);
+  const get_books_by_title = new Promise((resolve,reject) => {
+    const Title = req.query.title
+    resolve(books.filter(book => book.title === Title))
+  })
+  get_books_by_title.then((successMessage) => {
+    res.send(successMessage)
+  })
 });
 
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
  // Retrieve the email parameter from the request URL and send the corresponding friend's details
- const ISBN = req.query.isbn;
- const book = books.filter(book => book.isbn === ISBN);
- const review = book.map(Book => Book.reviews);
- res.send(review);
- res.send(os.EOL);
+ const get_reviews = new Promise((resolve,reject) => {
+  const ISBN = req.query.isbn;
+  resolve(books.filter(book => book.isbn === ISBN).map(Book => Book.reviews))
+ })
+ get_reviews.then((successMessage) => {
+  res.send(successMessage)
+ })
 });
 
 
